04f9dc26a19141a1b7f9f3ad466dd17ca1aea9ee
Greg Rundlett <greg@equality-tech.com>
2020-11-11 20:28:53 +0000

    Latest effort

--- resources/ext.adobeLaunch/init.js
+++ resources/ext.adobeLaunch/init.js
@@ -7,4 +7,12 @@
 	mw.adobeLaunch = {
 	};
 
+	console.log( "ready! Recording Page View\n" );
+	window._satellite.track('page_view',{
+		'site_id': 'FamilySearch',
+		'site_language': mw.config.get('wgContentLanguage'), // 'en'
+		'page_channel': 'Wiki',
+		'page_detail': document.location.pathname //'Home' for the homepage or a unique page title for other pages
+	});
+
 }() );
